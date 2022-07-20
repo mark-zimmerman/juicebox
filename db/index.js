2b73a0c6ea2b916105998bd558f6324886846c6b
Mark Zimmerman <markzimmerman1015@gmail.com>
2022-07-20 17:43:31 -0400

    part 2 almost finished

--- db/index.js
+++ db/index.js
@@ -10,28 +10,145 @@ module.exports = {
 }
 async function getAllUsers() {
     const { rows } = await client.query(
-        `SELECT id, username
+        `SELECT id, username, name, location, active
         FROM users;
         `);
     return rows;
 }
-async function createUser({ username, password }) {
-    try {
-      const result = await client.query(`
-        INSERT INTO users(username, password)
-        VALUES ($1, $2) ON CONFLICT (username) DO NOTHING 
-        RETURNING *;
-      `, [username, password]);
-  
-      return rows;
-    } catch (error) {
-      throw error;
-    }
+async function getAllPosts() {
+  try {
+    const { rows } = await client.query(
+      `SELECT "authorId", title, content, active`
+    );
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+async function createUser({ 
+  username, 
+  password,
+  name,
+  location
+}) {
+  try {
+    const { rows: [user] } = await client.query(`
+      INSERT INTO users(username, password, name, location) 
+      VALUES($1, $2, $3, $4) 
+      ON CONFLICT (username) DO NOTHING 
+      RETURNING *;
+    `, [username, password, name, location]);
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+async function createPost({
+  authorId,
+  title,
+  content
+}) {
+  try {
+    const { rows: [post] } = await client.query(`
+      INSERT INTO posts("authorId", title, content) 
+      VALUES($1, $2, $3) 
+      ON CONFLICT (title) DO NOTHING 
+      RETURNING *;
+    `, [authorId, title, content]);
+    return post;
+  } catch (error) {
+    throw error;
+  }
+}
+async function updateUser(id, fields = {}) {
+  // build the set string
+  const setString = Object.keys(fields).map(
+    (key, index) => `"${ key }"=$${ index + 1 }`
+  ).join(', ');
+
+  // return early if this is called without fields
+  if (setString.length === 0) {
+    return;
   }
 
+  try {
+    const { rows: [user]} = await client.query(`
+      UPDATE users
+      SET ${ setString }
+      WHERE id=${ id }
+      RETURNING *;
+    `, Object.values(fields));
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+async function updatePost(id, fields = {}) {
+  // build the set string
+  const setString = Object.keys(fields).map(
+    (key, index) => `"${ key }"=$${ index + 1 }`
+  ).join(', ');
+  // return early if this is called without fields
+  if (setString.length === 0) {
+    return;
+  }
+  
+  try {
+    const { rows: [post]} = await client.query(`
+      UPDATE posts
+      SET ${ setString }
+      WHERE id=${ id }
+      RETURNING *;
+    `, Object.values(fields));
+    return post;
+  } catch (error) {
+    throw error;
+  }
+}
+async function getPostsByUser(userId) {
+  console.log('getPostsByUser was invoked');
+  try {
+    const { rows } = await client.query(`
+      SELECT * FROM posts
+      WHERE "authorId"=${ userId };
+    `);
+
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+
+async function getUserById(userId) {
+  try {
+    const { rows } = await client.query(`
+      SELECT * FROM users
+      WHERE id=${userId};
+    `);
+    if (rows.length === 0) {
+      console.log('yoooo')
+      return null;
+    } else {
+      let userObj = rows[0];
+      delete userObj.password;
+      console.log(userObj);
+      posts = getPostsByUser(userId);
+      userObj['posts'] = posts;
+      return userObj;
+    }
+  } catch {
+
+  }
+}
+getUserById(2);
 //export them
 module.exports = {
     client,
     getAllUsers,
-    createUser
+    createUser,
+    updateUser, 
+    createPost
 }
